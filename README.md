c9051bceb9bfe0742930dd9128983c7c43f8d619
npybrozo <nina_py_brozovich@brown.edu>
2025-05-01 00:17:29 -0400

    Final writeup

--- README.md
+++ README.md
@@ -1,7 +1,117 @@
 # You just want Attention!
 
+Florencia Doval, Nina Py, Fabio Villatoro 26'
+
 ### A re-implementation of the Attention Is All You Need Paper in Pytorch with the Multi30k dataset
 
 ![Project Poster](static/Poster%20-%20You%20Just%20Want%20Attention%20Group.svg)
 
-Google Collab: https://colab.research.google.com/drive/1jkWL-MpFuMrmhAlenytciUatqugA5Oah?usp=sharing
+![Attention Heatmap](static/heatmap.jpg)
+
+#### Run the website (**[Watch Demo Video](static/demo.mp4)**):
+    > pip install -r requirements.txt
+    > python app.py
+    Note: you'll need to download the files from this drive folder: https://drive.google.com/drive/folders/1NlJ4XYmepTn1YKZCwr_-7Dg_suPsQ1Hl?usp=sharing
+    Save them in a folder called 'model' in the root of the project repo.
+
+#### Complete implementation:
+    All training code is in Pytorch_Multi30k_AIAYN.ipynb
+    
+    To run speech-to-text translation:
+    > python speech_translate.py
+    Note: you'll need to download the files from this drive folder: https://drive.google.com/drive/folders/1NlJ4XYmepTn1YKZCwr_-7Dg_suPsQ1Hl?usp=sharing
+    Save them in a folder called 'model' in the root of the project repo.
+
+
+Google Colab: https://colab.research.google.com/drive/1jkWL-MpFuMrmhAlenytciUatqugA5Oah?usp=sharing
+
+### Introduction
+
+We implemented the famous paper _Attention is all you need_ which innovated by proposing the Transformer, as a network architecture that relied only on Attention mechanisms and performed better in Natural Language Processing tasks than any other model at the time in 2017. They accomplished a 28.4 BLEU on the WMT 2014 English-to-German translation task. For our implementation we used the Multi30K dataset with image captions in German and in English.
+
+In addition to replicating the Transformer architecture, our project extends it with Whisper AI integration for speech-to-text translation. The complete pipeline accepts spoken German, transcribes it into text, translates it to English using our trained Transformer, and then synthesizes the output as speech using Google TTS for a complete cycle of language translation.
+
+This is a structured prediction problem, specifically sequence-to-sequence learning for machine translation, because the Transformer model takes an input sequence (English text) and produces a corresponding output sequence (German text). But can as well fall under the Supervised Learning category because we need clearly labeled input and output data in order to train and test our model’s accuracy.
+
+### Methodology
+
+We implemented a scaled-down version of the Transformer model in PyTorch. Initially, we followed the architecture as described in the paper but adapted it to the size of the Multi30k dataset, which consists of 30,000 image captions in German and English.
+
+We trained our model on Google Colab with an A100 GPU. Training took approximately 45 minutes per session.
+
+### Results
+
+At first, we implemented the exact same architecture as the paper _Attention is all you need_. Since we were getting extremely low learning rates and very high loss, we decided to decrease the size of our architecture to match the size of our model (significantly more reduced than the original dataset).
+
+<div style="display: flex; gap: 10px; align-items: center;">
+  <img src="static/old_architecture.png" alt="Old Architecture" width="300"/>
+  <img src="static/new_architecture.png" alt="New Architecture" width="300"/>
+</div>
+
+
+This allowed us to get a significantly higher learning rate, and smaller losses.
+
+**Final Metrics:**
+
+training loss = 1.8495
+
+validation loss = 2.2612
+
+![Loss Curve](static/loss.jpg)
+
+Additionally, we were able to get a BLEU score of **33.30**, which is higher than what we were expecting for our stretch goal (~25), and higher than the result the authors of the _Attention is all you need_ paper got on the WMT dataset. This is not a surprise, since the WMT dataset is around 133 times larger than ours and contains more general content.
+
+### Challenges
+
+It has been definitely hard to translate, reimplement, and reimagine such a big chunk of logic to pytorch. Elements like Multi-headed attention have been particularly hard.
+
+One of the most challenging parts of the implementation was to find a tokenization and hyperparameters that were appropriate for our data size to get good results. This was especially difficult because the model takes about 45 minutes to run with Google Colab’s A100 GPU (which slowed our ability to iterate and debug our code).
+
+Our model was not learning at first, and then learning too slowly and giving really wrong translations. After fine tunning, we got significantly better results as described above. Nevertheless, the small dataset led to occasional <unk> tokens and missed translations.
+
+Adapting our code and files to use Google Colab was also a bit challenging. We first had python files that we uploaded in the Runtime of Google Colab’s files folder every time we needed the model to run. This was not ideal for practicality, so we switched to notebook cells for the entirety of our code.
+
+### Reflection
+
+Throughout this project, we now understand better the importance of compute resources in DL. Using Google Colab's A100 GPU taught us to be strategic in our experimentation, and to planning each run given the training + evaluation takes almost an hour to run from beginning to end, which is costly both in time and money.
+
+We also improved our ability to read and interpret academic papers. Implementing the Transformer gave us hands-on experience with translating concepts into code.
+
+This experience also made us realize that we still have a long way to go to be able to dominate PyTorch, as well as implementing papers from scratch.
+
+
+#### How do you feel your project ultimately turned out? How did you do relative to your base/target/stretch goals? Did your model work out the way you expected it to?
+Our initial goals were:
+    
+    Base: Successfully implement and train a Transformer on 200,000 entries from the WMT 2014 dataset. Achieve a good BLEU score ( ≥10 ) on the test set.
+
+    Target: Fine tune hyperparameters to achieve a better BLEU score ( ≥20 ), comparable to smaller-scale benchmarks of the original paper.
+
+    Stretch: Improve BLEU score ( ≥25 ) through fine tuning, and evaluate on additional datasets. Use speech to text algorithms to create a complete translation software that can recognize speech and translate in real time. Time permitting, the translation will also be given in audio format to the user.
+
+Overall, the model worked as expected. We successfully implemented a Transformer model in PyTorch, trained it on a subset of the Multi30k dataset, and achieved a BLEU score of 33.30, which exceeded the threashold we had initially planned for our base, target and stretch goals. While some translations were not perfect and occasionally outputted <unk> tokens or skipped words, the results confirmed that our scaled-down model was learning meaningful sequence-to-sequence patterns. We also added the Whisper AI API for audio-to-text input/output for seamless DE-ENG translation. The thing we didn't get to do from our initial stretch goal was to evaluate our model on a completely different dataset. Looking back at it now, this might not necessarily work. Since the Multi30K dataset is composed uniquely of descriptive sentences, it might be hard to generalize to other kinds of inputs successfully.
+
+
+### How did your approach change over time? What kind of pivots did you make, if any?
+
+The major pivot in our implementation was the dataset we used for training. At first, we were planning on using 200,000 entries from the WMT 2014 dataset but switched to the Multi30K to satisfy the final project requirements. We chose this dataset because we found that people were able to train their transformers on the same dataset with good results in the past, and the size of the dataset would also allow us to train our model in an acceptable runtime given our computational constraints. We also shifted from coding Python on VSCode to running our entire pipeline in Google Colab notebooks to make use of A100 GPUs to train our model. This required us to adapt our code to run in a notebook. One challenge of using Colab is that the session's runtime often got disconnect, which forced us to run it several times before getting the final model.
+
+
+### What would you have done differently if you could do your project over again?
+
+If we were to do the project all over again, we would definitely study more in depth specifics of our dataset at an earlier stage of the project. For instance, if we had realized that training on captions would make our model less generalizable maybe we should have chosen a more general one. With our current knowledge, we might have chosen a slightly bigger one for better results too.
+
+If we were to start from scratch, we would have used a smaller architecture from the beginning to avoid wasting time adapting our architecture and running our code to get acceptable results. Maybe our results would have been even better at the end if we dedicated more time to fine tunning!
+
+Storing the model earlier in the training would have also help us test its functionality without having to run everything from scratch every time (which can take up a lot of time).
+
+### What do you think you can further improve on if you had more time?
+
+If we had more time to refine this project, we would have definitely tried to extend the model to larger datasets like WMT14 for better generalization, and similarly to what we did with our model, would have spent a significant amount of time fine tunning to get the best results we can achieve.
+
+Another area for improvement could be the tokenization and embedding strategies to handle rare words better and reduce <unk> tokens, hopefully leading to better results.
+
+
+### What are your biggest takeaways from this project/what did you learn?
+
+Our biggest takeaway was getting a hands-on understanding of how Transformers work by building one from scratch. We saw how important good preprocessing, tokenization, and fine-tuning are for getting acceptable results. Working with Colab also taught us to be smart about our code and experiments, since every run came with real time and cost trade-offs.
--- speech_translate.py
+++ speech_translate.py
@@ -5,7 +5,6 @@ import spacy
 import numpy as np
 import os
 from gtts import gTTS
-from IPython.display import Audio, display
 
 import json
 import pickle
@@ -20,13 +19,12 @@ PAD_TOKEN = '<pad>'
 UNK_TOKEN = '<unk>'
 
 # ---- Vocabulary Class ----
-class Vocabulary: #build vocabulary class to map tokens to unique indices
+class Vocabulary:
     def __init__(self, specials=None):
         self.token_to_idx = {}
         self.idx_to_token = {}
         self.frequencies = {}
 
-        #add special tokens like <unk>, <pad>, <sos>, <eos>
         if specials:
             for token in specials:
                 self.add_token(token, special=True)
@@ -75,14 +73,13 @@ class LearnablePositionalEncoding(nn.Module):
         self.pe = nn.Embedding(max_len, d_model)
         self.register_buffer('position_ids', torch.arange(max_len).expand((1, -1)))
 
-    def forward(self, x): #add positional embeddings to token embeddings
+    def forward(self, x):
         """
         x: (batch_size, seq_len, d_model)
         """
         position_ids = self.position_ids[:, :x.size(1)].to(x.device)
         return x + self.pe(position_ids)
 
-#multi-head self-attention layer
 class MultiHeadAttention(nn.Module):
     """
     Multi-Head Attention sublayer.
@@ -91,8 +88,6 @@ class MultiHeadAttention(nn.Module):
     by performing scaled dot-product attention in multiple heads, then
     concatenates the results.
     """
-    # allows the model to focus on different parts of the sequence
-
     def __init__(self, d_model, n_heads, dropout=0.1):
         super().__init__()
         assert d_model % n_heads == 0, "d_model must be divisible by n_heads."
@@ -101,29 +96,24 @@ class MultiHeadAttention(nn.Module):
         self.n_heads = n_heads
         self.head_dim = d_model // n_heads
 
-        #define linear layers for query, key, and value
         self.w_q = nn.Linear(d_model, d_model)
         self.w_k = nn.Linear(d_model, d_model)
         self.w_v = nn.Linear(d_model, d_model)
 
-        #output projection
         self.fc_out = nn.Linear(d_model, d_model)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, query, key, value, mask=None):
         B = query.size(0)
 
-        #apply linear projections
         Q = self.w_q(query)
         K = self.w_k(key)
         V = self.w_v(value)
 
-        #split into multiple heads
         Q = Q.view(B, -1, self.n_heads, self.head_dim).transpose(1, 2)
         K = K.view(B, -1, self.n_heads, self.head_dim).transpose(1, 2)
         V = V.view(B, -1, self.n_heads, self.head_dim).transpose(1, 2)
 
-        #compute scaled dot-product attention
         energy = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.head_dim)
 
         if mask is not None:
@@ -132,16 +122,13 @@ class MultiHeadAttention(nn.Module):
         attention = torch.softmax(energy, dim=-1)
         attention = self.dropout(attention)
 
-        #apply attention to values
         x = torch.matmul(attention, V)
         x = x.transpose(1, 2).contiguous()
         x = x.view(B, -1, self.d_model)
 
-        #final linear projection
         x = self.fc_out(x)
         return x, attention
 
-#position-wise feed-forward network
 class PositionwiseFeedForward(nn.Module):
     """
     Implements the two-layer feed-forward network used in the Transformer:
@@ -154,15 +141,13 @@ class PositionwiseFeedForward(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.relu = nn.ReLU()
 
-    def forward(self, x): #apply two linear transformations with relu activation
-        # Project up -> apply ReLU -> dropout -> project down
+    def forward(self, x):
         x = self.linear1(x)
         x = self.relu(x)
         x = self.dropout(x)
         x = self.linear2(x)
         return x
 
-#single encoder layer with self-attention and feed-forward
 class EncoderLayer(nn.Module):
     """
     Single Transformer encoder layer, made up of:
@@ -179,16 +164,13 @@ class EncoderLayer(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, src, src_mask=None):
-        #apply self-attention sublayer
         attn_output, _ = self.self_attn(src, src, src, mask=src_mask)
         src = self.norm1(src + self.dropout(attn_output))
 
-        #apply feed-forward sublayer
         ff_output = self.ff(src)
         src = self.norm2(src + self.dropout(ff_output))
         return src
 
-#encoder consisting of multiple encoder layers
 class Encoder(nn.Module):
     """
     Transformer Encoder that stacks multiple EncoderLayer layers.
@@ -197,37 +179,28 @@ class Encoder(nn.Module):
         super().__init__()
         self.d_model = d_model
 
-        #token embedding
         self.embed_tokens = nn.Embedding(input_dim, d_model)
 
-        #positional encoding
         self.pos_encoding = LearnablePositionalEncoding(d_model, max_len)
 
-        #dropout after embedding
         self.dropout = nn.Dropout(dropout)
 
-        #stack of EncoderLayers
         self.layers = nn.ModuleList([
             EncoderLayer(d_model, n_heads, dim_feedforward, dropout)
             for _ in range(n_layers)
         ])
 
     def forward(self, src, src_mask=None):
-        #embed tokens + scale
         src_embed = self.embed_tokens(src) * math.sqrt(self.d_model)
-        #add positional encoding
         src_embed = self.pos_encoding(src_embed)
-        #apply dropout
         src_embed = self.dropout(src_embed)
 
-        #pass through each layer
         out = src_embed
         for layer in self.layers:
             out = layer(out, src_mask)
 
         return out
 
-#single decoder layer with masked self-attention and encoder-decoder attention
 class DecoderLayer(nn.Module):
     """
     Single Transformer decoder layer, made up of:
@@ -237,9 +210,7 @@ class DecoderLayer(nn.Module):
     """
     def __init__(self, d_model, n_heads, dim_feedforward, dropout):
         super().__init__()
-        # Self-attention for the target tokens
         self.self_attn = MultiHeadAttention(d_model, n_heads, dropout)
-        # Cross-attention: query is the decoder, key/value is the encoder output
         self.cross_attn = MultiHeadAttention(d_model, n_heads, dropout)
         self.ff = PositionwiseFeedForward(d_model, dim_feedforward, dropout)
 
@@ -249,21 +220,17 @@ class DecoderLayer(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, tgt, enc_out, tgt_mask=None, src_mask=None):
-        # Masked self-attention
         self_attn_out, _ = self.self_attn(tgt, tgt, tgt, mask=tgt_mask)
         x = self.norm1(tgt + self.dropout(self_attn_out))
 
-        # Encoder-Decoder cross-attention
         cross_attn_out, attn_weights = self.cross_attn(x, enc_out, enc_out, mask=src_mask)
         x = self.norm2(x + self.dropout(cross_attn_out))
 
-        # Position-wise feed forward
         ff_out = self.ff(x)
         out = self.norm3(x + self.dropout(ff_out))
 
         return out, attn_weights
 
-#decoder consisting of multiple decoder layers
 class Decoder(nn.Module):
     """
     Transformer Decoder that stacks multiple DecoderLayer layers.
@@ -272,48 +239,37 @@ class Decoder(nn.Module):
         super().__init__()
         self.d_model = d_model
 
-        # Embedding for the target tokens
         self.embed_tokens = nn.Embedding(output_dim, d_model)
 
-        # Positional encoding
         self.pos_encoding = LearnablePositionalEncoding(d_model, max_len)
 
-        # Dropout after embedding
         self.dropout = nn.Dropout(dropout)
 
-        # Stack of DecoderLayers
         self.layers = nn.ModuleList([
             DecoderLayer(d_model, n_heads, dim_feedforward, dropout)
             for _ in range(n_layers)
         ])
 
-        # Final linear layer to map to output vocab
         self.fc_out = nn.Linear(d_model, output_dim, bias=False)
         self.embed_tokens = nn.Embedding(output_dim, d_model)
 
-        # Share weights
         self.fc_out.weight = self.embed_tokens.weight
 
     def forward(self, tgt, enc_out, tgt_mask=None, src_mask=None):
-        # Embed + scale
         tgt_embed = self.embed_tokens(tgt) * math.sqrt(self.d_model)
 
-        # Add position encodings
         tgt_embed = self.pos_encoding(tgt_embed)
         tgt_embed = self.dropout(tgt_embed)
 
-        # Pass through each layer
         out = tgt_embed
         attn_weights = None
         for layer in self.layers:
             out, attn_weights = layer(out, enc_out, tgt_mask, src_mask)
 
-        # Project hidden states to vocab logits
         logits = self.fc_out(out)
 
         return logits, attn_weights
 
-#complete transformer model combining encoder and decoder
 class Transformer(nn.Module):
     """
     Complete Transformer: an Encoder and a Decoder, with the ability to
@@ -339,12 +295,11 @@ class Transformer(nn.Module):
         self.decoder = Decoder(tgt_vocab_size, d_model, n_layers, n_heads,
                               dim_feedforward, dropout, max_len)
 
-        #initialize parameters with xavier uniform
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
 
-    def make_src_mask(self, src): #create mask for source to ignore pad tokens
+    def make_src_mask(self, src):
         """
         Creates a binary mask for the source sequence to ignore PAD tokens.
         """
@@ -358,10 +313,8 @@ class Transformer(nn.Module):
             2) apply subsequent/future masking so we don't look ahead.
         """
         B, tgt_len = tgt.shape
-        # Pad mask
         pad_mask = (tgt != self.tgt_pad_idx).unsqueeze(1).unsqueeze(2)
 
-        # Subsequent mask (no looking ahead)
         subsequent_mask = torch.tril(torch.ones((tgt_len, tgt_len), device=tgt.device)).bool()
         subsequent_mask = subsequent_mask.unsqueeze(0).unsqueeze(1)
 
@@ -378,7 +331,6 @@ class Transformer(nn.Module):
 
 # ---- Load Model ----
 def load_model(filename, device):
-    #checkpoint = torch.load(filename, map_location=device)
     checkpoint = torch.load(filename, map_location=device, weights_only=False)
 
     # Get vocabularies
@@ -446,16 +398,20 @@ def load_model_separately(weight_path, vocab_prefix, hyperparam_path, device):
 def translate_sentence(model, sentence, src_vocab, tgt_vocab, device, max_len=50, return_attention=False):
     model.eval()
 
+    #tokenize input sentence and add <sos>, <eos> tokens
     if isinstance(sentence, str):
         tokens = [SOS_TOKEN] + tokenize_de(sentence) + [EOS_TOKEN]
     else:
         tokens = [SOS_TOKEN] + sentence + [EOS_TOKEN]
 
+    #convert tokens to corresponding indices from source vocabulary
     src_indices = [src_vocab[token] for token in tokens]
     src_tensor = torch.LongTensor(src_indices).unsqueeze(0).to(device)
 
+    #pad mask for source sentence
     src_mask = model.make_src_mask(src_tensor)
 
+    #encode source sequence
     with torch.no_grad():
         enc_src = model.encoder(src_tensor, src_mask)
 
@@ -463,28 +419,32 @@ def translate_sentence(model, sentence, src_vocab, tgt_vocab, device, max_len=50
     outputs = []
     attentions = []
 
+    #translate one token at-a-time
     for _ in range(max_len):
         tgt_tensor = torch.LongTensor(tgt_indices).unsqueeze(0).to(device)
-        tgt_mask = model.make_tgt_mask(tgt_tensor)
+        tgt_mask = model.make_tgt_mask(tgt_tensor) #mask target sequence (no peeking ahead)
 
-        with torch.no_grad():
+        with torch.no_grad(): #decode with target and encoder output
             output, attention = model.decoder(tgt_tensor, enc_src, tgt_mask, src_mask)
 
-        output = output[:, -1, :]  # (batch_size=1, vocab_size)
-        pred_token = output.argmax(1).item()
+        output = output[:, -1, :] #get predicted token from last position
+        pred_token = output.argmax(1).item() #get index of highest-probability token
 
+        #save prediction and attention weights
         outputs.append(pred_token)
         attentions.append(attention)
 
+        #stop if model predicts <eos>
         if pred_token == tgt_vocab[EOS_TOKEN]:
             break
 
+        #append predicted token to target sequence
         tgt_indices.append(pred_token)
 
+    #convert predicted indices to tokens
     pred_tokens = [tgt_vocab.get_token(idx) for idx in outputs]
 
     if return_attention:
-        # return final step attention
         return pred_tokens, attentions[-1]
     else:
         return pred_tokens, None
@@ -498,21 +458,21 @@ def translate_custom_sentence(sentence):
         device=DEVICE
     )
 
-    # Tokenize the sentence
+    #tokenize sentence
     tokens = tokenize_de(sentence)
 
-    # Print original tokens
+    #print original tokens
     print(f"Original: {sentence}")
     print(f"Tokenized: {tokens}")
 
-    # Translate
+    #translate
     translation, attention = translate_sentence(loaded_model, tokens, loaded_src_vocab, loaded_tgt_vocab, DEVICE, return_attention=True)
 
-    # Remove EOS token if present
+    #remove EOS token if present
     if EOS_TOKEN in translation:
         translation = translation[:translation.index(EOS_TOKEN)]
 
-    # Print translation
+    #print translation
     print(f"Translation: {' '.join(translation)}")
 
     return translation, attention
@@ -573,4 +533,4 @@ if __name__ == "__main__":
     translate_custom_sentence("Zwei Hunde spielen im Garten.")
     translate_custom_sentence("Eine Katze schläft auf dem Sofa.")
 
-    #speech_to_speech_translation()
+    speech_to_speech_translation()
